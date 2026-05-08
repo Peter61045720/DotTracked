7242318301e5a1fcf95fea4d0611816827aef794
Peter61045720 <peter61045720@gmail.com>
2026-05-08 19:32:54 +0200

    Add Kanban board

---
+++ DotTracked.Shared/Enums/IssueCategory.cs
@@ -0,0 +1,7 @@
+﻿namespace DotTracked.Shared.Enums;
+
+public enum IssueCategory
+{
+    Personal = 0,
+    Group = 1
+}
---
+++ DotTracked/DotTracked.Client/Constants/KanbanColors.cs
@@ -0,0 +1,9 @@
+﻿namespace DotTracked.Client.Constants;
+
+public static class KanbanColors
+{
+    public const string DidNotComplete = "#f44336"; // Red
+    public const string NotStarted = "#ff9800"; // Orange
+    public const string InProgress = "#2196f3"; // Blue
+    public const string Done = "#4caf50"; // Green
+}
